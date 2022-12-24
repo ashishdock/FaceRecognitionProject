de783f372d75b26cf63f19668c638756a4c3b46e
Ashish Sharma <94269501+ashishdock@users.noreply.github.com>
2022-12-24 17:37:38 +0530

    Completed

--- FaceRecognitionProject/ViewController.swift
+++ FaceRecognitionProject/ViewController.swift
@@ -6,14 +6,39 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func signInButtonPressed(_ sender: UIButton) {
+        
+        let authContext = LAContext()
+        
+        var error: NSError?
+        
+        if authContext.canEvaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            authContext.evaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, localizedReason: "We need to scan your face to authenticate your login request.") { success, error in
+                if success == true {
+                    print("Auth successful.")
+                    DispatchQueue.main.async {
+                        self.performSegue(withIdentifier: "toSecondVC", sender: nil)
+                    }
+                } else {
+                    DispatchQueue.main.async {
+                        self.myLabel.text = "Error!"
+                        print(error)
+                    }
+                }
+            }
+        }
+    }
 }
 
